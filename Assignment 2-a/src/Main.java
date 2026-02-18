ac7a5a2debe26e0c904eedeeffdcff953c8d132c
johnleborne <johnleborne@gmail.com>
2026-02-18 12:00:39 -0600

    Update Main.java

--- Assignment 2-a/src/Main.java
+++ Assignment 2-a/src/Main.java
@@ -16,17 +16,14 @@ public class Main{
         search.linearSearch(nums,900);;
 
         System.out.println("\nBubble Sort:");
-        sort.writeSorted(sort.bubbleSort(nums), "sortednumb.txt");
-        //System.out.println(nums);
+        ArrayList<Integer> sortedNums = sort.bubbleSort(randomNumbers.getRandNums("RandNumb.txt"));
+        sort.writeSorted(sortedNums, "sortednumb.txt");
+        System.out.println(nums);
 
-        /*
-        When calling next sorting algorithms, use randomNumbers.getRandNums instead of nums as the input array
-        because nums becomes sorted after calling bubble sort, and the sorting algorithms need to be used on RandNumb.txt
-         */
         System.out.println("\nBinary Search:");
-        search.binarySearch(nums,1);
-        search.binarySearch(nums,500);
-        search.binarySearch(nums,900);
+        search.binarySearch(sortedNums,1);
+        search.binarySearch(sortedNums,500);
+        search.binarySearch(sortedNums,900);
 
 
 
